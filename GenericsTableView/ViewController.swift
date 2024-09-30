c79fa5affc4a99e8e31071cd6b410886cecd8a47
saadetsimsek <saadetsimsek13@gmail.com>
2024-09-30 16:10:51 +0300

    Initial Commit

---
+++ GenericsTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericsTableView
+//
+//  Created by Saadet Şimşek on 30/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GenericsTableViewTests/GenericsTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsTableViewTests.swift
+//  GenericsTableViewTests
+//
+//  Created by Saadet Şimşek on 30/09/2024.
+//
+
+import Testing
+@testable import GenericsTableView
+
+struct GenericsTableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
